8f32049bba27944d01240828162235651357382b
Bharat Hardi <bharathardi109@gmail.com>
2024-06-16 12:02:56 +0530

    method

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="https://formspree.io/f/xrgnnrje">
+      <form action="https://formspree.io/f/xrgnnrje" method="post">
          <div className="formGroup">
             <label htmlFor="name" hidden>
                Name
